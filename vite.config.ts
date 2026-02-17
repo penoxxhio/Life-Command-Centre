bf496572ae1ceb962d286c25b15c48932ee0322b
penoxxhio <abadey76@gmail.com>
2026-02-17 21:41:25 +0400

    Set base path for deployment in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
-
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  // Fix: Property 'cwd' does not exist on type 'Process'. Use type assertion to access Node.js process.cwd().
   const env = loadEnv(mode, (process as any).cwd(), '');
   return {
     plugins: [react()],
+    base: '/Life-Command-Centre/',
     define: {
       'process.env.API_KEY': JSON.stringify(env.API_KEY || '')
     },
