0e91a2505887ec97aaa15e9efe7b63e1ac1b317b
penoxxhio <abadey76@gmail.com>
2026-03-09 08:59:55 +0400

    feat(v2): rebuild - vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
-
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-export default defineConfig(({ mode }) => {
-  // Fix: Property 'cwd' does not exist on type 'Process'. Use type assertion to access Node.js process.cwd().
-  const env = loadEnv(mode, (process as any).cwd(), '');
-  return {
-    plugins: [react()],
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.API_KEY || '')
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    build: {
-      outDir: 'dist',
-      sourcemap: false
-    }
-  };
+  },
 });
