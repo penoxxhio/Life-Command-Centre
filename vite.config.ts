29374dcb8a3aff030c36c882359bc0c452c42ac9
penoxxhio <abadey76@gmail.com>
2026-03-10 11:42:12 +0400

    v3 rebuild: add vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,14 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    minify: 'esbuild',
+    target: 'esnext',
+  },
 });
